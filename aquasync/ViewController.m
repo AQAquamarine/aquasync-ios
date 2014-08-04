dc1af86fee38c14bbffd49816d1add7945e138cb
Kai INUI <lied.der.optik@gmail.com>
2014-08-05 01:07:52 +0900

    try code

--- aquasync/ViewController.m
+++ aquasync/ViewController.m
@@ -22,6 +22,19 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     NSLog(@"%@", [AQUtil getDeviceToken]);
+    
+    
+    RLMRealm *realm = [RLMRealm defaultRealm];
+    [realm beginWriteTransaction];
+    [realm deleteObjects: [FLMAlbum allObjects]];
+    [realm commitWriteTransaction];
+    
+    FLMAlbum *album1 = [[FLMAlbum alloc] init];
+    [album1 save];
+    
+    FLMAlbum *album2 = [[FLMAlbum alloc] init];
+    [album2 save];
+    
     [AQDeltaClient sharedInstance].baseURI = @"http://0.0.0.0:3000/api/v1/";
     [[AQDeltaClient sharedInstance] setBasicAuthorizationWithUsername:@"hogehoge" password:@"hogehoge"];
     AQSyncManager *manager = [AQSyncManager sharedInstance];
