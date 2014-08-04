ded0996089de3d495010c76307f06eaa03a6191a
Kai INUI <lied.der.optik@gmail.com>
2014-08-04 23:52:33 +0900

    do not contain isDirty in pushing delta

--- aquasync/RLMObject+Dictionary.m
+++ aquasync/RLMObject+Dictionary.m
@@ -5,6 +5,7 @@
 - (NSDictionary *)aq_toDictionary {
     NSMutableDictionary *dic = [[NSMutableDictionary alloc] init];
     for (RLMProperty *p in self.objectSchema.properties) {
+        if([p.name isEqual:@"isDirty"]){continue;}
         dic[p.name] = [self valueForKey:p.name];
     }
     return dic;
