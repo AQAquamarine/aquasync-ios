18a0efcc28e8d4008af0b3bdac8225c120e8c345
Kai INUI <lied.der.optik@gmail.com>
2014-10-30 22:43:39 +0900

    init

---
+++ Aquasync/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Aquasync
+//
+//  Created by kaiinui on 2014/10/30.
+//  Copyright (c) 2014年 Aquamarine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '7.0'
+
+target "Aquasync" do
+  pod "AFNetworking"
+end
+
+target "AquasyncTests" do
+  pod "Expecta"
+  pod "OHHTTPStubs"
+  pod "OCMock"
+end
