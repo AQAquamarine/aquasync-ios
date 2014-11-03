d0861789e6ec6ccb7640ae91bb4b77f8a8f1d92d
Kai INUI <lied.der.optik@gmail.com>
2014-11-03 18:08:25 +0900

    Implement AQDeltaPack

--- Aquasync/Classes/AQDeltaPack.h
+++ Aquasync/Classes/AQDeltaPack.h
@@ -8,9 +8,42 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NSDictionary AQDelta;
+
 /**
  *  A data class that represents DeltaPack described in https://github.com/AQAquamarine/aquasync-protocol/blob/master/deltapack.md
  */
-@interface AQDeltaPack : NSOperation
+@interface AQDeltaPack : NSObject
+
+# pragma mark - Inserting Deltas
+/** @name Inserting Deltas */
+
+/**
+ *  Insert a delta for model.
+ *
+ *  @param delta A delta.
+ *  @param key   A key that specifies the model for the delta.
+ */
+- (void)addDelta:(AQDelta *)delta forKey:(NSString *)key;
+
+/**
+ *  Insert deltas from array.
+ *
+ *  @param array An array that contains deltas. The type should be `AQDelta` as known as `NSDictionary`.
+ *  @param key   A key that specifies the model for the delta.
+ */
+- (void)addDeltasFromArray:(NSArray /* <AQDelta *> */ *)array forKey:(NSString *)key;
+
+# pragma mark - Getting Deltas
+/** @name Getting Deltas */
+
+/**
+ *  Obtain an array that contains deltas that paired with given key.
+ *
+ *  @param key A key that specifies the model.
+ *
+ *  @return An array that contains deltas that paired with given key. The type is `AQDelta` as known as `NSDictionary`.
+ */
+- (NSArray /* <AQDelta> */ *)arrayForKey:(NSString *)key;
 
 @end
--- Aquasync/Classes/AQDeltaPack.m
+++ Aquasync/Classes/AQDeltaPack.m
@@ -8,6 +8,47 @@
 
 #import "AQDeltaPack.h"
 
+@interface AQDeltaPack ()
+
+@property (nonatomic, strong) NSMutableDictionary *actualDictionary;
+
+@end
+
 @implementation AQDeltaPack
+
+# pragma mark - Initialization
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _actualDictionary = [[NSMutableDictionary alloc] init];
+    }
+    return self;
+}
+
+# pragma mark - Inserting Deltas
+
+- (void)addDelta:(AQDelta *)dictionary forKey:(NSString *)key {
+    [[self deltaArrayForKey:key] addObject:dictionary];
+}
+
+- (void)addDeltasFromArray:(NSArray *)array forKey:(NSString *)key {
+    [[self deltaArrayForKey:key] addObjectsFromArray:array];
+}
+
+# pragma mark - Getting Deltas
+
+- (NSArray *)arrayForKey:(NSString *)key {
+    return [self deltaArrayForKey:key];
+}
+
+# pragma mark - Helpers
+
+- (NSMutableArray *)deltaArrayForKey:(NSString *)key {
+    if (!self.actualDictionary[key]) {
+        self.actualDictionary[key] = [[NSMutableArray alloc] init];
+    }
+    return self.actualDictionary[key];
+}
 
 @end
